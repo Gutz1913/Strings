31dd284a74b2a6d1c708ff18cdcf495d0cd5ba9a
Bacudo1991 <andresgutierrez1135155@correo.itm.edu.co>
2026-02-16 17:26:20 -0500

    Working with Strings

---
+++ Strings/Strings.UI.Console/UserInterface.cs
@@ -0,0 +1,14 @@
+﻿using Strings.Core;
+
+var myStrings = new MyString[]
+{
+    new MyString("A mamá Roma le aviva el amor a papá y a papá Roma le aviva el amor a mamá"),
+    new MyString("Anita, la gorda lagartona, no traga la droga latina"),
+    new MyString("Madam, I'm Adam"),
+    new MyString("Oye, sí. Versos revisé yo"),
+};
+
+foreach (var myString in myStrings)
+{
+    Console.WriteLine(myString.IsPalindrome());
+}
